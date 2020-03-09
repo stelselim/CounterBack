c45693ee1b88e996bbf5eda776272bf50ecb692d
Selim <selim.ustel1998@gmail.com>
2020-03-09 15:02:35 +0300

    Main page added and appbar created

---
+++ lib/Pages/mainpage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+class MainBody extends StatelessWidget {
+  const MainBody({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    );
+  }
+}
---
+++ lib/components/appbar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+
+
+AppBar mainAppBar({@required String appBarTitle}){
+
+  return AppBar(
+    title: Text(appBarTitle),
+    actions: <Widget>[
+
+    ],
+    centerTitle: true,
+  );
+
+}
